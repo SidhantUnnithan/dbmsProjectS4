44c1691003afa8898b73d272863fc286e95d5d37
Sidhant Unnithan <sidhantunnithan@gmail.com>
2020-04-01 00:37:29 +0530

    Added files via upload

---
+++ dbmsAdmin.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/914f58546c.js" crossorigin="anonymous"></script>
+	<title>Booking (*_*) </title>
+</head>
+<body style="background: rgb(255, 194, 195);">
+	<nav class="navbar navbar-dark" style="background:rgb(232, 0, 0);">
+		<div class="container">
+			<a class="navbar-brand"><i class="fas fa-comment-dollar mr-3"></i>Booking</a>
+			<ul class="nav navbar-nav navbar-right">
+				<li style="font-weight: bold;"><a href="#">admin</a></li>
+			</ul>
+		</div>	
+	</nav>
+
+	<div class = "container" style="margin-top: 30px">
+		<table class="table table-striped">
+		    <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+                <th scope="col">email</th>
+                <th scope="col">movie</th>
+                <th scope="col">no of tickets</th>
+                <th scope="col">time</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <script language="PHP">
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "password";
+                    $dbname = "dbms";
+                    
+                    if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+                        echo 'We don\'t have mysqli!!!';
+                    } else {
+                        echo 'Phew we have it!<br>';
+                    }                
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "SELECT f_name, l_name, email_id, movie_id, no_of_tickets FROM customer";
+	                $result = $conn->query($sql);
+                    
+                    $n = 1;
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                                <tr>
+                                    <th scope="row"><?php echo($n) ?></th>
+                                        <td><?php echo($row["f_name"]) ?></td>
+                                        <td><?php echo($row["l_name"]) ?></td>
+                                        <td><?php echo($row["email_id"]) ?></td>
+                                        <td><?php echo($row["movie_id"]) ?></td>
+                                        <td><?php echo($row["no_of_tickets"]) ?></td>
+                                        <td>21:30</td>
+                                </tr>
+                            $n = $n + 1;
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                </script>                   
+                    
+            </tbody>    
+		</table>
+	</div>
+
+</body>
+</html>
---
+++ dbmsProject.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/914f58546c.js" crossorigin="anonymous"></script>
+	<title>
+		Booking XD
+	</title>
+</head>
+<body style="background: rgb(255, 194, 195);">
+	
+	<h3 style="margin-left: 70px ; margin-top: 20px;">ENTER THE DETAILS</h3>
+	<div style="margin-left: 70px; margin-top: 10px; margin-right: 70px";>
+		<form action="dbmsprojdet1.php" method="get">
+ 			 <div class="row">
+ 			    <div class="col">
+ 			     <input type="text" class="form-control" placeholder="First name" name="f_name">
+ 			    </div>
+                
+                <div class="col">
+ 			     <input type="text" class="form-control" placeholder="Last name" name="l_name">
+ 			    </div>
+ 			 </div>
+              
+            <div class="form-group row" style="margin-top: 10px;">
+    			<label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
+    			<div class="col-sm-10">
+    			  <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="email" name="email_id">
+    			</div>
+  			</div>
+              
+            <label class="my-1 mr-2" for="inlineFormCustomSelectPref">movie</label>
+  			<select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref" name="movie">
+  			  <option selected>Choose...</option>
+  			  <option value="STAR WARS THE CLONE WARS">star wars : the clone wars</option>
+  			  <option value="ZOMBIELAND DOUBLE TAP">zombie land: double tap</option>
+  			  <option value="1917">1917</option>
+  			  <option value="JOKER">jocker</option>
+  			</select>
+              
+  			<div class="col-lg-6">
+  				<label class="my-1 mr-2" for="inlineFormCustomSelectPref">No of Tickets</label>
+  				<br>
+  					<input type="number" class="form-control" min="1" max="10" name="NoT">
+  				</br>
+  			</div>	
+  			</div>
+  			</div>
+
+  			<div style="margin-top: 20px;">
+ 				<input type="submit" class="btn btn-primary" value="Submit">			
+ 			</div> 
+		</form>
+
+		<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "password";
+			$dbname = "dbms";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+			$fname = $_GET["f_name"];
+			$lname = $_GET["l_name"];
+			$email = $_GET["email_id"];
+			$movie = $_GET["movie"];
+			$NoT = $_GET["NoT"];
+			
+			$sql = "SELECT movie_id FROM movie WHERE movie_name='$movie'";
+        	$result = $conn->query($sql);
+
+        	if ($result->num_rows > 0) {
+            	// output data of each row
+            	while($row = $result->fetch_assoc()) {                
+                	$movie_id = $row["movie_id"];
+            	}
+        	}
+			
+			$sql = "INSERT ignore into customer VALUES ('$fname', '$lname', '$email', $movie_id, $NoT)";
+
+			if (mysqli_query($conn, $sql)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+			
+			$conn->close();
+    	?> 
+	</div>	
+
+</body>
+</html>
