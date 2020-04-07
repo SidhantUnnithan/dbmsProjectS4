674956a5da349a2ad7e4f39400d5ccf25b2f2bdb
SidhantUnnithan <sidhantunnithan@gmail.com>
2020-04-08 01:01:03 +0530

    Linked, added favicon

--- dbmsProject.php
+++ dbmsProject.php
@@ -4,13 +4,15 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://kit.fontawesome.com/914f58546c.js" crossorigin="anonymous"></script>
 	<title>
-		Booking XD
+		Booking
 	</title>
+	<link rel = "icon" href = "https://freepngimg.com/download/popcorn/23057-2-popcorn-transparent-image.png">
 </head>
 <body style="background: rgb(255, 194, 195);">
 	
-	<h3 style="margin-left: 70px ; margin-top: 20px;">ENTER THE DETAILS</h3>
+	
 	<div style="margin-left: 70px; margin-top: 10px; margin-right: 70px";>
+		<h3 style="text-align: center; margin-top: 20px; margin-bottom: 20px;"><strong>Enter Your Details</strong></h3>
 		<form action="dbmsprojdet1.php" method="get">
  			 <div class="row">
  			    <div class="col">
@@ -20,16 +22,14 @@
                 <div class="col">
  			     <input type="text" class="form-control" placeholder="Last name" name="l_name">
  			    </div>
- 			 </div>
+			  </div>
+			  
+			  <label class="my-2 mr-2" for="inlineFormCustomSelectPref">Email</label>
+			<br>
+				<input type="email" class="form-control" name="email_id" placeholder="example@example.com"> 
+			</br>
               
-            <div class="form-group row" style="margin-top: 10px;">
-    			<label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
-    			<div class="col-sm-10">
-    			  <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="email" name="email_id">
-    			</div>
-  			</div>
-              
-            <label class="my-1 mr-2" for="inlineFormCustomSelectPref">movie</label>
+            <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Movie</label>
   			<select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref" name="movie">
   			  <option selected>Select a Movie</option>
   			  <option value="Parasite" <?= $_GET['movie'] == "Parasite" ? "selected = selected" : ""; ?>>Parasite</option>
